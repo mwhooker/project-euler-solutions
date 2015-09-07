5fe3f831dd1a91473e7f9c65e84711ca98453c3a
Matthew Hooker <mwhooker@gmail.com>
2015-09-07 00:55:47 -0700

    solved

--- 26.py
+++ 26.py
@@ -1,23 +1,22 @@
 from decimal import *
 
-def get_cycle(offset, s):
+def get_cycle(s):
     for j in xrange(1, len(s)/2):
-        if s[offset:j] == s[j+offset:(j*2)+offset]:
+        if s[:j] == s[j:j*2]:
             return j
-    return None
 
 if __name__ == '__main__':
-    getcontext().prec = 200
-    found = []
+    getcontext().prec = 2000
+    answer = (0,0)
     one = Decimal(1)
     for i in xrange(1, 1000):
         part = str(one / Decimal(i))[2:]
-        for j in xrange(len(part)/2):
-            cycle = get_cycle(j, part)
-            if cycle:
-                found.append((cycle, i))
+        for k in xrange(len(part)):
+            cycle = get_cycle(part[k:])
+            # print part[k:], cycle
+            if cycle > answer[0]:
+                answer = (cycle, i)
 
-    print found
-    answer = max(found)
-    print "found %s. its sequence is %s digits long" % tuple(reversed(answer))
+    print answer
+    print "found %s. its sequence is %s digits long" % (answer[1], answer[0])
     print one / Decimal(answer[1])
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SRCDIR  := .
-FILES := $(shell find $(SRCDIR) -type f -name "*.py")
+FILES := $(shell find $(SRCDIR) -not -path venv -type f -name "*.py")
 
 test:
 	nosetests --with-doctest $(FILES)
